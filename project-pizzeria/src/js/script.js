835a5033863b4db0794020e6438ffea677f275ed
Hanna Boratynska <boratynskahanna@gmail.com>
2021-03-30 10:48:06 +0400

    Correct accordion

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -113,7 +113,6 @@
           }
         }
 
-
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
@@ -138,7 +137,6 @@
         event.preventDefault();
         thisProduct.processOrder();
       });
-      
     }
     
 
@@ -157,7 +155,6 @@
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         
-    
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
@@ -174,8 +171,6 @@
               price -= option.price;
             }
           }
-
-          
         }
       }
     
